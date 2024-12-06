5882bc0f2f35f4a8fd26587094361deb94da7360
maxime but on his ratfuckd debian laptop <mjochum@student.42perpignan.fr>
2024-12-06 17:49:32 +0100

    saveState

--- srcs/mariaDB/config/startupScript.sh
+++ srcs/mariaDB/config/startupScript.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+mysql_install_db --user=root --datadir=/etc/mariaDB --skip-test-db;
 
-$_MDBUSER="mjochum"
-$_MDBPASSWORD="mjochum"
-$_MDBNAME="mjochum"
+mysqld_safe;
 
-mysql_install_db --user=root --datadir=/etc/mariaDB --skip-test-db
-mysqld_safe
-mysqld --user=root
+mysqladmin -uroot create $_MDBNAME;
+#CREATE DATABASE your_database_name;
 
-echo "SLIP"
-sleep 100000
+mysql -uroot -e "CREATE USER '$_MDBUSER' IDENTIFIED BY '$_MDBPASSWORD';";
+
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $_MDBNAME.* TO '$_MDBUSER';";
+
+# mysqladmin -uroot create $_MDBNAME
+# mysqladmin -uroot -e 'IN DATABASE $_MDBNAME'
+
+mysqld --user=root;
