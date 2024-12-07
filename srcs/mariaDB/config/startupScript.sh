1c1a09e6bc18e8dad805c07164d8235cd6897729
maxime but on his ratfuckd debian laptop <mjochum@student.42perpignan.fr>
2024-12-07 18:13:59 +0100

    saveState_itWorksOnMariaDbJustGottaFigureOutHowToHaveItRun

--- srcs/mariaDB/config/startupScript.sh
+++ srcs/mariaDB/config/startupScript.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-mysql_install_db --user=root --datadir=/etc/mariaDB --skip-test-db;
+echo "------- MARIADB.sh run as:" $(whoami);
 
-mysqld_safe;
+mysql_install_db --user=root --datadir=/etc/mariaDB --skip-test-db --auth-root-authentication-method=normal
 
-mysqladmin -uroot create $_MDBNAME;
+
+mysqld_safe --user=root &
+
+while [ ! -S "/run/mysqld/mysqld.sock" ]; do
+    echo "hihi hoho"
+    sleep 1
+done
+
+echo "------- Peeping the socket:";
+ls /run/mysqld/
+
+echo "------- check"
+mysql -uroot -e "SHOW DATABASES"
+
+echo "------- create db CMD"
+mysqladmin -uroot create $_MDBNAME
 #CREATE DATABASE your_database_name;
 
-mysql -uroot -e "CREATE USER '$_MDBUSER' IDENTIFIED BY '$_MDBPASSWORD';";
+echo "------- create user nd pw CMD"
+mysql -uroot -e "CREATE USER '$_MDBUSER' IDENTIFIED BY '$_MDBPASSWORD';"
 
-mysql -uroot -e "GRANT ALL PRIVILEGES ON $_MDBNAME.* TO '$_MDBUSER';";
+echo "------- grant privileges CMD"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $_MDBNAME.* TO '$_MDBUSER';"
 
-# mysqladmin -uroot create $_MDBNAME
-# mysqladmin -uroot -e 'IN DATABASE $_MDBNAME'
-
-mysqld --user=root;
+echo "------- Peeping the socket:";
+ls /run/mysqld/
+echo "END"
