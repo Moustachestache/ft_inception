1b05aec99ffd1738bb86eeece07670e3b3a879a9
maxime but on his ratfuckd debian laptop <mjochum@student.42perpignan.fr>
2024-12-09 16:46:40 +0100

    saveState

--- srcs/wordpress/config/install.sh
+++ srcs/wordpress/config/install.sh
@@ -2,15 +2,19 @@
 
 # it's all here
 # https://make.wordpress.org/cli/handbook/guides/installing/
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
+# edit php-fdm to be on port 9001
+RUN		sed -i 's/listen = 127.0.0.1:9000/listen = 9001/g' /etc/php82/php-fpm.d/www.conf
 
 # php at /etc/php83/php.ini
 # do nothing?
 # https://make.wordpress.org/cli/handbook/guides/quick-start/
 
 cd /var/www/
+
 
 # check if files
 wp core download
@@ -19,9 +23,10 @@ wp core download
 
 wp config create --dbname=$_MDBNAME --dbuser=$_MDBUSER --dbpass=$_MDBPASSWORD --dbhost=mariadb --allow-root
 
-
 # check if db
-wp db create
+#wp db create
 
 # chek if installed
 wp core install
+
+# set port 9001
